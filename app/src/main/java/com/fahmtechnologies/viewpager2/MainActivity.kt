ca6d106182e43c5c3ebb99af29c7fb06462d6573
Sakib Syed <sakib.fahm@gmail.com>
2019-12-13 11:23:26 +0530

    All files commited......Sakib Syed

--- app/src/main/java/com/fahmtechnologies/viewpager2/MainActivity.kt
+++ app/src/main/java/com/fahmtechnologies/viewpager2/MainActivity.kt
@@ -2,13 +2,14 @@ package com.fahmtechnologies.viewpager2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.fahmtechnologies.viewpager2.Fragment.OneFragment
 import com.fahmtechnologies.viewpager2.Fragment.ThreeFragment
 import com.fahmtechnologies.viewpager2.Fragment.TwoFragment
-import com.fahmtechnologies.viewpager2.ViewPagerIndicator.ZoomOutPageTransformer
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -16,20 +17,29 @@ class MainActivity  : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
         val fragmentList : ArrayList<Fragment>  = arrayListOf(
             OneFragment.newInstance(),TwoFragment.newInstance(),  ThreeFragment.newInstance())
 
         val pagerAdapter = ScreenSlidePagerAdapter(this,fragmentList)
-
-        val zoomOutPageTransformer = ZoomOutPageTransformer()
-        viewPager2.setPageTransformer { page, position ->
-            zoomOutPageTransformer.transformPage(page, position)
-        }
-
-
         viewPager2.adapter = pagerAdapter
+
+
+//        val zoomOutPageTransformer = ZoomOutPageTransformer()
+//        viewPager2.setPageTransformer { page, position ->
+//            zoomOutPageTransformer.transformPage(page, position)
+//        }
+
+        springDotsIndicator.setViewPager2(viewPager2)
+
+
+//        val springDotsIndicator = findViewById<SpringDotsIndicator>(R.id.springDotsIndicator)
+//        springDotsIndicator.setViewPager2(viewPager2)
     }
 
     override fun onBackPressed() {
